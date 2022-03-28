f8a5944b6ae2b232169452b4305716979ddd692d
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-03-28 15:41:46 +0200

    Temporarily complete the menu bar

--- app/src/main/java/com/and/netshare/MainActivity.java
+++ app/src/main/java/com/and/netshare/MainActivity.java
@@ -1,6 +1,13 @@
 package com.and.netshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,13 +16,20 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.and.netshare.login.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth mainAuth;
-
+    private FirebaseAuth mainAuth;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    private Toolbar toolbar;
+    private NavController navController;
+    private AppBarConfiguration configuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
         mainAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = mainAuth.getCurrentUser();
         checkUser(firebaseUser);
+        initView();
+        setupNavigation();
+    }
+
+    private void initView(){
+        drawerLayout = findViewById(R.id.main_drawer);
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        toolbar = findViewById(R.id.topBar);
+    }
+
+    private void setupNavigation(){
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        setSupportActionBar(toolbar);
+
+        configuration = new AppBarConfiguration.Builder(R.id.homePageFragment)
+                .setOpenableLayout(drawerLayout).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, configuration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        setBottomNavigationView();
+    }
+
+    private void setBottomNavigationView(){
+        navController.addOnDestinationChangedListener(((navController1, navDestination, bundle) -> {
+            final int id = navDestination.getId();
+            if (id == R.id.homePageFragment  || id == R.id.userFragment){
+                bottomNavigationView.setVisibility(View.VISIBLE);
+            } else {
+                bottomNavigationView.setVisibility(View.GONE);
+            }
+        }));
     }
 
     private void checkUser(FirebaseUser currentUser){
--- app/src/main/java/com/and/netshare/login/LoginActivity.java
+++ app/src/main/java/com/and/netshare/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.and.netshare.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -13,19 +16,31 @@ import com.and.netshare.login.ui.main.LoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavController navController;
+    private AppBarConfiguration configuration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, LoginFragment.newInstance())
-                    .commitNow();
-        }
-        NavController navController = Navigation.findNavController(this, R.id.nav_login);
-        NavigationUI.setupActionBarWithNavController(this, navController);
+
+        initView();
+        setupNavigation();
     }
 
+    private void initView(){
+        drawerLayout = findViewById(R.id.login_drawer);
+        toolbar = findViewById(R.id.login_topbar);
+    }
+
+    private void setupNavigation(){
+        navController = Navigation.findNavController(this, R.id.nav_login);
+        setSupportActionBar(toolbar);
+
+        configuration = new AppBarConfiguration.Builder(R.id.homePageFragment)
+                .setOpenableLayout(drawerLayout).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, configuration);
+    }
 
 }
