f5b7300f17a80585daffc1209a3b8bd3cdd5c1f2
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-03-30 22:34:36 +0200

    modify the buttons of items in user page but not set the style and functions(do this 31st March)

--- app/src/main/java/com/and/netshare/home/userlist/UserFragment.java
+++ app/src/main/java/com/and/netshare/home/userlist/UserFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.and.netshare.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,13 +20,10 @@ import java.util.ArrayList;
 
 public class UserFragment extends Fragment {
 
-    private RecyclerView userSettingList;
-    private UserAdapter userAdapter;
     private TextView userEmail;
     private TextView userName;
     private FirebaseAuth userAuth;
     private FirebaseUser currentUser;
-    private ArrayList<UserItem> items = new ArrayList<>();
 
     public UserFragment() {
         // Required empty public constructor
@@ -38,7 +36,6 @@ public class UserFragment extends Fragment {
         currentUser = userAuth.getCurrentUser();
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -46,21 +43,8 @@ public class UserFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_user, container, false);
         userEmail = v.findViewById(R.id.user_email);
         userName = v.findViewById(R.id.user_name);
-        userEmail.setText(currentUser.getEmail());
         userName.setText(currentUser.getDisplayName());
-
-        userSettingList = v.findViewById(R.id.user_list_view);
-        userSettingList.hasFixedSize();
-        userSettingList.setLayoutManager(new LinearLayoutManager(getContext()));
-
-        // Arraylist for all the items
-        items.add(new UserItem(getString(R.string.user_item1), R.drawable.account_icon));
-        items.add(new UserItem(getString(R.string.user_item2), R.drawable.about_icon));
-        items.add(new UserItem(getString(R.string.user_item3), R.drawable.settings_icon));
-        items.add(new UserItem(getString(R.string.user_item4), R.drawable.logout_icon));
-
-        userAdapter = new UserAdapter(items);
-        userSettingList.setAdapter(userAdapter);
+        userEmail.setText(currentUser.getEmail());
         return v;
     }
 }
