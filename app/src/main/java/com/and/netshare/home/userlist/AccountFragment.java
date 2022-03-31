8423954dfedc80ed6c776c9bffc2697e63ce02e6
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-03-31 18:16:27 +0200

    account done

--- app/src/main/java/com/and/netshare/home/userlist/AccountFragment.java
+++ app/src/main/java/com/and/netshare/home/userlist/AccountFragment.java
@@ -1,23 +1,35 @@
 package com.and.netshare.home.userlist;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.and.netshare.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class AccountFragment extends Fragment {
 
     private EditText userName;
     private TextView userEmail;
+    private Button editUsername;
+    private Button updatePassword;
 
     private FirebaseAuth userAuth;
     private FirebaseUser currentUser;
@@ -41,6 +53,46 @@ public class AccountFragment extends Fragment {
         userEmail = v.findViewById(R.id.userEmail);
         userName.setText(currentUser.getDisplayName());
         userEmail.setText(currentUser.getEmail());
+        editUsername = v.findViewById(R.id.account_change_name);
+        updatePassword = v.findViewById(R.id.change_password);
+
+        editUsername.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(userName.getText().toString())
+                        .build();
+
+                currentUser.updateProfile(profileChangeRequest)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Log.d(TAG, "User profile updated.");
+                                    Log.d("update username", userName.getText().toString());
+                                    Snackbar.make(v, R.string.account_setUsername, Snackbar.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        updatePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userAuth.sendPasswordResetEmail(currentUser.getEmail())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Log.d(TAG, "Reset password email sent.");
+
+                                    Snackbar.make(v, R.string.account_resetPassword, Snackbar.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
         return v;
     }
 }
