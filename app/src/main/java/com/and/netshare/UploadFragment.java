bb69d6402af31891a757842e784334b5b83b69d4
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-04-06 00:30:55 +0200

    upload working

--- app/src/main/java/com/and/netshare/UploadFragment.java
+++ app/src/main/java/com/and/netshare/UploadFragment.java
@@ -8,10 +8,12 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -25,6 +27,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 
 public class UploadFragment extends Fragment {
@@ -33,11 +42,13 @@ public class UploadFragment extends Fragment {
     private Button choose_camera;
     private Button upload;
     private ImageView preview;
+    private Uri imageURI;
+    private FirebaseStorage storage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        storage = FirebaseStorage.getInstance();
     }
 
     @Override
@@ -45,6 +56,8 @@ public class UploadFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_upload, container, false);
+        StorageReference storageRef = storage.getReference();
+
         choose_camera = v.findViewById(R.id.choose_camera);
         choose_local = v.findViewById(R.id.choose_local);
         upload = v.findViewById(R.id.upload);
@@ -58,6 +71,42 @@ public class UploadFragment extends Fragment {
                 startActivityForResult(local, 2);
             }
         });
+
+        choose_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preview.setDrawingCacheEnabled(true);
+                preview.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) preview.getDrawable()).getBitmap();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                byte[] data = outputStream.toByteArray();
+
+                StorageReference localRef = storageRef.child(imageURI.getLastPathSegment() + ".jpeg");
+                UploadTask uploadTask = localRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Log.v("failed", "upload failed");
+                        // Handle unsuccessful uploads
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.v("success", "upload success");
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                        // ...
+                    }
+                });
+            }
+        });
         return v;
     }
 
@@ -65,10 +114,13 @@ public class UploadFragment extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 2) {
             if (data != null) {
-                Uri uri = data.getData();
-                Log.i("uri of image", uri.toString());
+                imageURI = data.getData();
+                Log.i("uri of image", imageURI.getPath());
+                Log.i("uri of image", imageURI.getLastPathSegment());
+                Log.i("uri of image", imageURI.getEncodedPath());
+                Log.i("uri of image", imageURI.getPathSegments().toString());
 
-                preview.setImageURI(uri);
+                preview.setImageURI(imageURI);
             }
         } else {
             //operation error or not select image
