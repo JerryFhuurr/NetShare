4d1b70657fafe3bbd3f68ccf033aaee8d1bfe9e6
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-04-09 00:06:50 +0200

    modify register func

--- app/src/main/java/com/and/netshare/login/ui/main/RegisterFragment.java
+++ app/src/main/java/com/and/netshare/login/ui/main/RegisterFragment.java
@@ -10,6 +10,7 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,22 +21,29 @@ import android.widget.Toast;
 
 import com.and.netshare.MainActivity;
 import com.and.netshare.R;
+import com.and.netshare.home.userlist.DataHandler;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
 
 public class RegisterFragment extends Fragment {
 
     private EditText email;
+    private EditText userName;
     private EditText password;
     private EditText password_repeat;
+
     private Button login;
     private Button ok;
     private FirebaseAuth registerAuth;
-    private FirebaseUser newUser;
+    private FirebaseDatabase db;
+    private DatabaseReference newUser;
     private TextView error_label;
     public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";   //verify email
 
@@ -44,17 +52,24 @@ public class RegisterFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseDatabase.getInstance("https://netshare-f4723-default-rtdb.asia-southeast1.firebasedatabase.app");
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_register, container, false);
         email = v.findViewById(R.id.register_email);
+        userName = v.findViewById(R.id.register_username);
         password = v.findViewById(R.id.register_password);
         password_repeat = v.findViewById(R.id.register_password_repeat);
         login = v.findViewById(R.id.register_change);
         ok = v.findViewById(R.id.register_ok);
         registerAuth = FirebaseAuth.getInstance();
-        newUser = registerAuth.getCurrentUser();
+        newUser = db.getReference("UserEmail/");
         error_label = v.findViewById(R.id.error_label);
         return v;
     }
@@ -73,22 +88,29 @@ public class RegisterFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 String emailString = email.getText().toString();
+                String userNameString = userName.getText().toString();
                 String password1 = password.getText().toString();
                 String password2 = password_repeat.getText().toString();
 
-                if (emailString.matches(REGEX_EMAIL) && (password1.trim()).equals(password2.trim()) && password1.length() > 6) {
-                    registerAuth.createUserWithEmailAndPassword(emailString, password1)
-                            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()){
-                                        Toast.makeText(getContext(), R.string.register_info, Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(getActivity(), MainActivity.class));
+                try {
+                    if (emailString.matches(REGEX_EMAIL) && (password1.trim()).equals(password2.trim()) && password1.length() >= 6) {
+                        registerAuth.createUserWithEmailAndPassword(emailString, password1)
+                                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            newUser.child(DataHandler.changeDotToComaEmail(emailString)).setValue(userNameString);
+                                            Toast.makeText(getContext(), R.string.register_info, Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(getActivity(), MainActivity.class));
+                                        }
                                     }
-                                }
-                            });
-                } else {
-                    error_label.setText(R.string.register_error);
+                                });
+
+                    } else {
+                        error_label.setText(R.string.register_error);
+                    }
+                }catch (Exception e){
+                    Log.d("register eoor ", e.getMessage());
                 }
             }
         });
@@ -104,7 +126,6 @@ public class RegisterFragment extends Fragment {
             reload();
         }
     }
-
 
     private void reload() {
         Toast.makeText(getContext(), R.string.login_toast1, Toast.LENGTH_SHORT).show();
