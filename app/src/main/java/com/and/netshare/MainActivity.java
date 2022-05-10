3315afb3032a9c5ebf4b714ee4acc0873a3ec85f
JerryFhuurr <79422097+JerryFhuurr@users.noreply.github.com>
2022-05-10 16:25:05 +0200

    test commit

--- app/src/main/java/com/and/netshare/MainActivity.java
+++ app/src/main/java/com/and/netshare/MainActivity.java
@@ -76,7 +76,6 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences =
                 PreferenceManager.getDefaultSharedPreferences(this);
         appThemeLoad(sharedPreferences, this);
-
         SoundHandler.initSound(this);
 
         setSupportActionBar(toolbar);
